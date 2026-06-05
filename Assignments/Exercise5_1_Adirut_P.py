d09e7bf86b4a0e7b5ffaa795230576f014bcc331
mixercat <mixercat47@gmail.com>
2026-06-05 11:41:14 +0700

    Create Exercise5_1_Adirut_P.py

---
+++ Assignments/Exercise5_1_Adirut_P.py
@@ -0,0 +1,7 @@
+x = int(input("Please enter a number 1 : "))
+y = int(input("Please enter a number 2 : "))
+
+print(x, "+", y, "=", x + y)
+print(x, "-", y, "=", x - y)
+print(x, "*", y, "=", x * y)
+print(x, "/", y, "=", int(x / y))
